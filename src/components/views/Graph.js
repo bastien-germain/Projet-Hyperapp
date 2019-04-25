9a35b80ef56ad720fe2b772ed1d4986021ffd2b2
jeremyrts <jeremy.ratsimandresy@gmail.com>
2019-04-25 16:09:34 +0200

    getting the coordinates with the state

--- src/components/views/Graph.js
+++ src/components/views/Graph.js
@@ -1,25 +1,25 @@
 import { h } from 'hyperapp'
 import Chart from 'chart.js';
 
-export default (state, actions) => {
+export default (props) => {
 
-  var ville = state.citiesSaved[0];
+  var ville = props.savedCities
   console.log(ville)
-  var coordinates = [ville.longitude, ville.latitude];
+  var coordinates = [ville[0].longitude, ville[0].latitude]
   console.log(coordinates)
-  var info = actions.getOldWeatherData(coordinates);
-  console.log(info)
+  // var info = actions.getOldWeatherData(coordinates);
+  // console.log(info)
 
+  // Fonction qui récupère les informations anciennes à partir de l'api
 
   return (
     <div>
-      <h2>Issy-les-Moulineaux</h2>
       <canvas
         oncreate={element => {
           var myChart = new Chart(element.id, {
             type: 'line',
             data: {
-              labels: ['1990', '1995', '2000', '2005', '2010', '2015'], // Name of elements 
+              labels: ['1990', '1995', '2000', '2005', '2010', '2015'], // Labels of time 
               datasets: [{
                 label: 'Evolution de la température sur les 10 dernières années',
                 data: [12, 19, 3, 5, 2, 3], // The temperatures
