2f3953f827a154e922e6fde2cc5aabcd33a6c0db
jeremyrts <jeremy.ratsimandresy@gmail.com>
2019-05-13 22:57:17 +0200

    First attemps at getting previous forecast, still stuck in use a loop and using second parameters to the function

--- src/views/components/CityResults.js
+++ src/views/components/CityResults.js
@@ -6,8 +6,17 @@ const CityResults = (props) => {
   return (
     <div class="cityResults">
     {props.cities.map( city =>
-      <p onclick={() => props.getWeatherData(city.geometry.coordinates)
-        .then(data => props.createCity({name: city.properties.name, ...data}))}>
+      <p onclick={() => {props.getWeatherData(city.geometry.coordinates)
+        .then(data => props.createCity({ name: city.properties.name, ...data}))
+        // let targetDate = Math.round(Date.now() / 1000)
+        // console.log(targetDate)
+        //for (let index = 0; index < 5; index++) {
+          // targetDate -= (63072000 * index)
+          // console.log(targetDate)
+          props.getPreviousForecast(city.geometry.coordinates, 1)
+            .then(data => props.addPreviousForecast(data))
+       // }
+        }}>
         {city.properties.name} - {city.properties.postcode}
       </p>
     )}
