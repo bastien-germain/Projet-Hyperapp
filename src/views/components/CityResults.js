e98f222f558e8df4504a157328c28542d21f90b6
jeremyrts <jeremy.ratsimandresy@gmail.com>
2019-05-16 23:24:54 +0200

    Delete comments

--- src/views/components/CityResults.js
+++ src/views/components/CityResults.js
@@ -9,7 +9,6 @@ const CityResults = (props) => {
       <p onclick={() => {props.getWeatherData(city.geometry.coordinates)
         .then(data => props.createCity({ name: city.properties.name, ...data}))
         let targetDate = Math.round(Date.now() / 1000)
-        // console.log(targetDate)
         for (let i = 0; i < 6; i++) {
           targetDate -= (63072000 * i)
           props.getPreviousForecast({coordinates: city.geometry.coordinates, time: targetDate})
